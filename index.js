5f739b33da823b30b58105aea11973636828f642
Dibbo <divyajitchakraborty@gmail.com>
2025-12-11 02:26:15 +0600

    Added Tuition Patch

--- index.js
+++ index.js
@@ -44,23 +44,14 @@ async function run() {
         console.log("Pinged MongoDB ...");
 
 
-        //client.newuser >>> server >>> database
-        app.post('/users', async (req, res) => {
-            const user = req.body;
-            user.isAdmin = false;
-            user.createdAt = new Date();
-            const email = user.email;
-            const userExists = await userCollection.findOne({ email })
-
-            if (userExists) {
-                return res.send({ message: 'user exists already...' })
-            }
-
-            const result = await userCollection.insertOne(user);
-            res.send(result);
+        //***get APIS***
+        //client.users <<< server <<< database
+        app.get(`/users`, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.json(users);
         })
 
-        //client.tutors <<< server <<< database
+        //client.users <<< server <<< database
         app.get(`/tutors`, async (req, res) => {
             const tutors = await tutorsCollection.find().toArray();
             res.json(tutors);
@@ -89,7 +80,6 @@ async function run() {
                         message: "Cant Find this id"
                     })
                 }
-
                 res.json(tuition);
             }
             catch (err) {
@@ -99,10 +89,30 @@ async function run() {
         })
 
 
+
+        //***post APIS***
+        //client.newuser >>> server >>> database
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            user.isAdmin = false;
+            user.createdAt = new Date();
+            const email = user.email;
+            const userExists = await userCollection.findOne({ email })
+
+            if (userExists) {
+                return res.send({ message: 'user exists already...' })
+            }
+
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+
         //client.newtuition >>> server >>> database
         app.post(`/newtuition`, async (req, res) => {
             const newTuition = req.body;
-            newTuition.paymentStatus = 'pending';
+            newTuition.paymentStatus = 'Pending';
+            newTuition.approvalStatus = 'Pending';
             newTuition.image = `https://dummyimage.com/600x400/000/fff.png&text=${newTuition.subject}`
             newTuition.createdAt = new Date();
 
@@ -112,9 +122,37 @@ async function run() {
 
 
 
+        //***patch APIS */
+        app.patch('/tuitions/:id', async (req, res) => {
+            const { id } = req.params;
+            const updatedData = req.body;
+            try {
+                if (!ObjectId.isValid(id)) {
+                    return res.status(400).json({ error: "Invalid ID format" });
+                }
+                const updated = await tuitionsCollection.updateOne(
+                    { _id: new ObjectId(id) },
+                    { $set: updatedData }
+                );
+                res.json({
+                    message: "Tuition updated",
+                    updated
+                });
+            }
+            catch (err) {
+                console.error("error:", err);
+                res.status(500).json({ error: "Failed" });
+            }
+        });
 
-    } finally {
+
+
     }
+
+    //errorhandle
+    catch { }
+    finally { }
+
 }
 
 
