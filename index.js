a64700d27e6d0800871ee4a7d4a9358bd51205fe
Dibbo <divyajitchakraborty@gmail.com>
2025-12-11 21:16:13 +0600

    Added Tuition Application as Tutor

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const cors = require('cors');
 const app = express();
-// require('dotenv').config();
+require('dotenv').config();
 
-//stripe
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 3000
 const crypto = require("crypto");
+const { messaging } = require('firebase-admin');
+const stripe = require('stripe')(process.env.STRIPE_KEY);
+
 
 //jwt
 // const admin = require("firebase-admin");
@@ -17,7 +19,7 @@ const crypto = require("crypto");
 
 
 //mongoDB
-const uri = "mongodb+srv://7SD7KDbrcLsiVkCw:7SD7KDbrcLsiVkCw@etuition.lmeq1nq.mongodb.net/?appName=etuition";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@etuition.lmeq1nq.mongodb.net/?appName=etuition`;
 
 //midleware 
 app.use(express.json());
@@ -39,12 +41,14 @@ async function run() {
         const tuitionsCollection = db.collection('tuitions');
         const tutorsCollection = db.collection('tutors');
         const paymentsCollection = db.collection('payments');
+        const applicationsCollection = db.collection('applications');
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged MongoDB ...");
 
-
+        //-----------------------------------------------------------------
         //***get APIS***
+
         //client.users <<< server <<< database
         app.get(`/users`, async (req, res) => {
             const users = await userCollection.find().toArray();
@@ -90,7 +94,9 @@ async function run() {
 
 
 
+        //-----------------------------------------------------------------
         //***post APIS***
+
         //client.newuser >>> server >>> database
         app.post('/users', async (req, res) => {
             const user = req.body;
@@ -120,9 +126,64 @@ async function run() {
             res.send(result);
         })
 
+        //payment API
+        app.post(`/checkout`, async (req, res) => {
+
+            const tuitionInfo = req.body;
+            const fee = tuitionInfo.fee;
+            const session = await stripe.checkout.sessions.create({
+                line_items: [
+                    {
+                        price_data: {
+                            currency: 'bdt',
+                            unit_amount: fee,
+                            product_data: {
+                                name: `Payment for: ${tuitionInfo.subject}`
+                            }
+                        },
+                        quantity: 1,
+                    },
+                ],
+                mode: 'payment',
+                metadata: {
+                    tuitionId: tuitionInfo._id
+                },
+                customer_email: tuitionInfo.creatorEmail,
+                success_url: `${process.env.SITE_URL}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+                cancel_url: `${process.env.SITE_URL}/payment-cancelled`,
+            })
+
+            res.send({
+                url: session.url
+            })
+        })
+
+        //application for tuition
+        app.post(`/apply`, async (req, res) => {
+
+            const application = req.body;
+            
+            try {
+                const result = await applicationsCollection.insertOne(application);
+                res.status(201).send({
+                    message: "Submitted",
+                    insertedId: result.insertedId,
+                })
+            } catch (error) {
+                console.log(error);
+                res.status(500).send({
+                    message: 'failed'
+                })
+            }
+        })
 
 
+
+
+
+        //-----------------------------------------------------------------
         //***patch APIS */
+
         app.patch('/tuitions/:id', async (req, res) => {
             const { id } = req.params;
             const updatedData = req.body;
@@ -147,12 +208,37 @@ async function run() {
 
 
 
-    }
 
+        //-----------------------------------------------------------------
+        // ***delete APIS***
+        app.delete(`/tuitions/delete/:id`, async (req, res) => {
+            const { id } = req.params;
+
+            try {
+                const result = await tuitionsCollection.deleteOne({
+                    _id: new ObjectId(id)
+                });
+                if (result.deletedCount === 0) {
+                    return res.status(404).json({ message: "no tuition found" });
+                }
+
+                res.json({ message: "deleted successfully", deletedCount: result.deletedCount });
+
+            } catch (err) {
+                console.error("Error:", err);
+                res.status(400).json({ message: "Invalid format or server error." });
+            }
+        });
+
+
+
+
+
+
+    }
     //errorhandle
     catch { }
     finally { }
-
 }
 
 
